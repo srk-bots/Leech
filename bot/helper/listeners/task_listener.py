f922ac424c5301a6ef83c886bf6335be23b3fb19
Shadoworbs <saieedadam101@gmail.com>
2025-05-02 02:57:25 +0000

    fix: enhance completion message to include bot username

--- bot/helper/listeners/task_listener.py
+++ bot/helper/listeners/task_listener.py
@@ -383,7 +383,7 @@ class TaskListener(TaskConfig):
         ):
             await database.rm_complete_task(self.message.link)
         msg = f"<b>Name: </b><code>{escape(self.name)}</code>\n\n<b>Size: </b>{get_readable_file_size(self.size)}"
-        done_msg = f"{self.tag}\nYour task is complete\nPlease check your inbox."
+        done_msg = f"{self.tag}\nYour task is complete\nPlease check your inbox.\n\n👉 <b>{Config.BOT_USERNAME}</b>"
         LOGGER.info(f"Task Done: {self.name}")
         if self.is_leech:
             msg += f"\n<b>Total Files: </b>{folders}"
