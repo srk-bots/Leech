ac772e9a98d960d7dd43750acd64543bdadc1de7
srk-bots <sakthisivam1610@gmail.com>
2025-05-19 09:44:38 +0530

    Add files via upload

---
+++ manual_handler.py
@@ -0,0 +1,3 @@
+async def handle_manual(client, message):
+    # Parse flags like -resize, -compress, -burn_sub, -metadata, -name_swap, etc.
+    await message.reply("Manual comment feature will be implemented here.")
